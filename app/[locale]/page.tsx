b5990cacd9eaa86e42f69753549d2218aaa6e066
Lenner <Grayson.Lenner@quartech.com>
2024-02-13 20:16:45 -0800

    keycloak sign in forced before app starts

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -4,6 +4,43 @@ import { ChatbotUISVG } from "@/components/icons/chatbotui-svg"
 import { IconArrowRight } from "@tabler/icons-react"
 import { useTheme } from "next-themes"
 import Link from "next/link"
+import Keycloak from "keycloak-js"
+import { httpClient } from "./utils/HttpClient"
+
+//KEYCLOAK CONFIG
+//////////////////////////////////////////////////////////////////////////
+let initOptions = {
+  url: "https://sso.trustservices.quartech.com/", // Provide a default value if undefined
+  realm: "trust-services", // Provide a default value if undefined
+  clientId: "chatApp" // Provide a default value if undefined
+}
+let kc = new Keycloak(initOptions)
+kc.init({
+  onLoad: "login-required",
+  checkLoginIframe: false, //maybe true?? supposed to check signin silently without page reload but causes reload currently
+  pkceMethod: "S256"
+}).then(
+  auth => {
+    if (!auth) {
+      window.location.reload()
+    } else {
+      console.log("Authenticated") //remove logs in production!!
+      console.log("auth", auth)
+      console.log("Keycloak", kc)
+      console.log("Access Token", kc.token)
+
+      httpClient.defaults.headers.common["Authorization"] = `Bearer ${kc.token}`
+
+      kc.onTokenExpired = () => {
+        console.log("token expired")
+      }
+    }
+  },
+  () => {
+    console.error("Authentication Failed!")
+  }
+)
+///////////////////////////////////////////////////////////////////////////
 
 export default function HomePage() {
   const { theme } = useTheme()
